6a27aaae03fa4122074c79d13f16388c3c3897c3
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-16 20:06:05 +0000

    added post delete method to delete urls

--- express_server.js
+++ express_server.js
@@ -51,6 +51,12 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURl);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
