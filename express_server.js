9c1947f04ecf976cd1c306b4a981dc3a58a277cc
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-16 23:31:31 +0000

    installed and required cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 const PORT = 8080; //default port 8080
 
 const getRandomChar = (string) => {
