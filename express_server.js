84170232cfbd50939f4c077638b1ee7ad742e8c4
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-17 00:40:34 +0000

    updated header html for format

--- express_server.js
+++ express_server.js
@@ -22,8 +22,8 @@ const generateRandomString = (callback) => {
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  // "b2xVn2": "http://www.lighthouselabs.ca",
+  // "9sm5xK": "http://www.google.com"
   
 };
 
