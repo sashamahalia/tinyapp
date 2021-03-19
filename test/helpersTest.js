cc1e5c83c8c632dbd6930a213a2ee7d5c9702b45
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-19 01:04:43 +0000

    created test folderdirectory, tested helper function

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+const { getUserByEmail } = require('../helpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', () => {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    assert.strictEqual(user, expectedOutput);
+  });
+  it('should return undefined for a user with an invalid email', () => {
+    const user = getUserByEmail("user3@example.com", testUsers);
+    const expectedOutput = undefined;
+    assert.strictEqual(user, expectedOutput);
+  });
+});
